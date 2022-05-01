1c0ccf3660b954df314254821805bed5865b3b7e
tatar0071 <50038800+tatar0071@users.noreply.github.com>
2022-05-01 09:51:52 +0300

    Update esp.lua

--- Modules/esp.lua
+++ Modules/esp.lua
@@ -3,6 +3,7 @@ local Workspace = game:GetService("Workspace")
 local Camera = Workspace.CurrentCamera
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
+local CoreGui = game:GetService("CoreGui")
 
 -- Framework
 local Framework = {}; Framework.__index = Framework; do
@@ -14,6 +15,13 @@ local Framework = {}; Framework.__index = Framework; do
     end
     function Framework:Draw(Object, Properties)
         Object = Drawing.new(Object)
+        for Property, Value in pairs(Properties) do
+            Object[Property] = Value
+        end
+        return Object
+    end
+    function Framework:Instance(Object, Properties)
+        Object = Instance.new(Object)
         for Property, Value in pairs(Properties) do
             Object[Property] = Value
         end
@@ -40,13 +48,15 @@ local ESP = {
     Settings = {
         Enabled = false,
         Maximal_Distance = 1000,
-        Highlight = {Enabled = false, Color = Color3.new(1, 0, 0)},
-        Box = {Enabled = false, Color = Color3.new(1, 1, 1)},
-        Box_Outline = {Enabled = false, Color = Color3.new(0, 0, 0), Outline_Size = 1},
-        Healthbar = {Enabled = false, Position = "Left", Color = Color3.new(1, 1, 1), Color_Lerp = Color3.fromRGB(40, 252, 3)},
-        Name = {Enabled = false, Position = "Top", Color = Color3.new(1, 1, 1), OutlineColor = Color3.new(0, 0, 0)},
-        Distance = {Enabled = false, Position = "Bottom", Color = Color3.new(1, 1, 1), OutlineColor = Color3.new(0, 0, 0)},
-        Tool = {Enabled = false, Position = "Right", Color = Color3.new(1, 1, 1), OutlineColor = Color3.new(0, 0, 0)}
+        Highlight = {Enabled = false, Color = Color3.new(1, 0, 0), Target = ""},
+        Box = {Enabled = false, Color = Color3.new(1, 1, 1), Transparency = 1},
+        Box_Outline = {Enabled = false, Color = Color3.new(0, 0, 0), Transparency = 1, Outline_Size = 1},
+        Healthbar = {Enabled = false, Position = "Left", Color = Color3.new(1, 1, 1), Transparency = 1, Color_Lerp = Color3.fromRGB(40, 252, 3)},
+        Name = {Enabled = false, Position = "Top", Color = Color3.new(1, 1, 1), Transparency = 1, OutlineColor = Color3.new(0, 0, 0)},
+        Distance = {Enabled = false, Position = "Bottom", Color = Color3.new(1, 1, 1), Transparency = 1, OutlineColor = Color3.new(0, 0, 0)},
+        Tool = {Enabled = false, Position = "Right", Color = Color3.new(1, 1, 1), Transparency = 1, OutlineColor = Color3.new(0, 0, 0)},
+        Health = {Enabled = false, Position = "Right", Transparency = 1, OutlineColor = Color3.new(0, 0, 0)},
+        Chams = {Enabled = false, Color = Color3.new(1, 1, 1), OutlineColor = Color3.new(0, 0, 0), Transparency = 0.5, OutlineTransparency = 0}
     },
     Objects = {},
     Overrides = {}
@@ -86,11 +96,15 @@ do -- Player Metatable
     Player_Metatable.__index = Player_Metatable
     function Player_Metatable:Destroy()
         ESP.Objects[self.Object] = nil
-        for Index, Drawing in pairs(self.Components) do
-            Drawing.Visible = false
-            Drawing:Remove()
-            self.Components[Index] = nil
+        for Index, Component in pairs(self.Components) do
+            if Index == "Chams" then
+                Component:Destroy()
+                return
+            end
+            Component.Visible = false
+            Component:Remove()
         end
+        self.Components[Index] = nil
     end
     function Player_Metatable:Update()
         local Box, Box_Outline = self.Components.Box, self.Components.Box_Outline
@@ -99,15 +113,19 @@ do -- Player Metatable
         local Distance = self.Components.Distance
         local Tool = self.Components.Tool
         local Health = self.Components.Health
+        local Chams = self.Components.Chams
+        if Box == nil or Box_Outline == nil or Healthbar == nil or Healthbar_Outline == nil or Name == nil or Distance == nil or Tool == nil or Health == nil or Chams == nil then
+            self:Destroy()
+        end
         local Character = Get_Character(self.Player)
-        if Character then
+        if Character ~= nil then
             local Head, HumanoidRootPart, Humanoid = Character:FindFirstChild("Head"), Character:FindFirstChild("HumanoidRootPart"), Character:FindFirstChildOfClass("Humanoid")
             if not Humanoid then
                 self:Destroy()
                 return
             end
-            local Health, Health_Maximum = Humanoid.Health, Humanoid.MaxHealth
-            if Head and HumanoidRootPart and Health > 0 then
+            local Current_Health, Health_Maximum = Humanoid.Health, Humanoid.MaxHealth
+            if Head and HumanoidRootPart and Current_Health > 0 then
                 local Dimensions = Framework:Get_Bounding_Vectors(HumanoidRootPart)
                 local HRP_Position, On_Screen = Camera:WorldToViewportPoint(HumanoidRootPart.Position)
                 local Stud_Distance, Meter_Distance = math.floor(HRP_Position.Z + 0.5), math.floor(HRP_Position.Z / 3.5714285714 + 0.5)
@@ -136,6 +154,9 @@ do -- Player Metatable
                 local Box_Position = Framework:Round_V2(Vector2.new(X_Maximal + Box_Size.X / X_Minimal, Y_Maximal + Box_Size.Y / Y_Minimal))
 
                 if ESP.Settings.Enabled and On_Screen and Meter_Distance < ESP.Settings.Maximal_Distance then
+                    local Is_Highlighted = ESP.Settings.Highlight.Enabled and ESP.Settings.Highlight.Target == self.Object or false
+                    local Highlight_Color = ESP.Settings.Highlight.Color
+
                     -- Offsets
                     local Top_Offset = 3
                     local Bottom_Offset = Y_Maximal + 1
@@ -145,28 +166,31 @@ do -- Player Metatable
                     -- Box
                     Box.Size = Box_Size
                     Box.Position = Box_Position
-                    Box.Color = ESP.Settings.Box.Color
+                    Box.Color = Is_Highlighted and Highlight_Color or ESP.Settings.Box.Color
+                    Box.Transparency = ESP.Settings.Box.Transparency
                     Box.Visible = ESP.Settings.Box.Enabled
 
                     Box_Outline.Size = Box_Size
                     Box_Outline.Position = Box_Position
                     Box_Outline.Color = ESP.Settings.Box_Outline.Color
-                    Box_Outline.Visible = ESP.Settings.Box_Outline.Enabled
                     Box_Outline.Thickness = ESP.Settings.Box_Outline.Outline_Size + 2
+                    Box_Outline.Transparency = ESP.Settings.Box_Outline.Transparency
+                    Box_Outline.Visible = ESP.Settings.Box.Enabled and ESP.Settings.Box_Outline.Enabled or false
 
                     -- Healthbar
                     local Health_Top_Size_Outline = Vector2.new(Box_Size.X - 4, 3)
                     local Health_Top_Pos_Outline = Box_Position + Vector2.new(2, Box_Size.Y - 6)
-                    local Health_Top_Size_Fill = Vector2.new((Health * Health_Top_Size_Outline.X / Health_Maximum) + 2, 1)
+                    local Health_Top_Size_Fill = Vector2.new((Current_Health * Health_Top_Size_Outline.X / Health_Maximum) + 2, 1)
                     local Health_Top_Pos_Fill = Health_Top_Pos_Outline + Vector2.new(1 + -(Health_Top_Size_Fill.X - Health_Top_Size_Outline.X),1);
 
                     local Health_Left_Size_Outline = Vector2.new(3, Box_Size.Y - 4)
                     local Health_Left_Pos_Outline = Vector2.new(X_Maximal + Box_Size.X - 6, Box_Position.Y + 2)
-                    local Health_Left_Size_Fill = Vector2.new(1, (Health * Health_Left_Size_Outline.Y / Health_Maximum) + 2)
+                    local Health_Left_Size_Fill = Vector2.new(1, (Current_Health * Health_Left_Size_Outline.Y / Health_Maximum) + 2)
                     local Health_Left_Pos_Fill = Health_Left_Pos_Outline + Vector2.new(1,-1 + -(Health_Left_Size_Fill.Y - Health_Left_Size_Fill.Y));
 
                     local Healthbar_Enabled = ESP.Settings.Healthbar.Enabled
                     local Healthbar_Position = ESP.Settings.Healthbar.Position
+                    local Health_Lerp_Color = ESP.Settings.Healthbar.Color:Lerp(ESP.Settings.Healthbar.Color_Lerp, Current_Health / Health_Maximum)
                     if Healthbar_Enabled then
                         if Healthbar_Position == "Left" then
                             Healthbar.Size = Health_Left_Size_Fill;
@@ -191,7 +215,7 @@ do -- Player Metatable
                             Healthbar_Outline.Position = Health_Top_Pos_Outline - Vector2.new(0, Box_Size.Y - 9)
                             Bottom_Offset = Bottom_Offset + 6
                         end
-                        Healthbar.Color = ESP.Settings.Healthbar.Color:Lerp(ESP.Settings.Healthbar.Color_Lerp, Health / Health_Maximum)
+                        Healthbar.Color = Health_Lerp_Color
                     end
                     Healthbar.Visible = Healthbar_Enabled
                     Healthbar_Outline.Visible = Healthbar_Enabled
@@ -219,7 +243,7 @@ do -- Player Metatable
                         end
                         Right_Offset = Right_Offset + 10
                     end
-                    Name.Color = ESP.Settings.Name.Color
+                    Name.Color = Is_Highlighted and Highlight_Color or ESP.Settings.Name.Color
                     Name.OutlineColor = ESP.Settings.Name.OutlineColor
                     Name.Visible = ESP.Settings.Name.Enabled
 
@@ -247,7 +271,7 @@ do -- Player Metatable
                         Right_Offset = Right_Offset + 10
                     end
                     Distance.Text = Meter_Distance.."m"
-                    Distance.Color = ESP.Settings.Distance.Color
+                    Distance.Color = Is_Highlighted and Highlight_Color or ESP.Settings.Distance.Color
                     Distance.OutlineColor = ESP.Settings.Distance.OutlineColor
                     Distance.Visible = ESP.Settings.Distance.Enabled
 
@@ -275,9 +299,49 @@ do -- Player Metatable
                         Right_Offset = Right_Offset + 10
                     end
                     Tool.Text = Get_Tool(self.Player)
-                    Tool.Color = ESP.Settings.Tool.Color
+                    Tool.Color = Is_Highlighted and Highlight_Color or ESP.Settings.Tool.Color
                     Tool.OutlineColor = ESP.Settings.Tool.OutlineColor
                     Tool.Visible = ESP.Settings.Tool.Enabled
+
+                    -- Health
+                    local Health_Position = ESP.Settings.Health.Position
+                    if Health_Position == "Top" then 
+                        Health.Position = Vector2.new(X_Maximal + Box_Size.X / 2, Box_Position.Y) - Vector2.new(0, Health.TextBounds.Y - Box_Size.Y + Top_Offset) 
+                        Top_Offset = Top_Offset + 10
+                    elseif Health_Position == "Bottom" then
+                        Health.Position = Vector2.new(Box_Size.X / 2 + Box_Position.X, Bottom_Offset) 
+                        Bottom_Offset = Bottom_Offset + 10
+                    elseif Health_Position == "Left" then
+                        if Healthbar_Position == "Left" then
+                            Health.Position = Health_Left_Pos_Outline - Vector2.new(Health.TextBounds.X/2 - 2 + 4, -(100 * Health_Left_Size_Outline.Y / 100) + 2 - Left_Offset)
+                        else
+                            Health.Position = Health_Left_Pos_Outline - Vector2.new(Health.TextBounds.X/2 - 2, -(100 * Health_Left_Size_Outline.Y / 100) + 2 - Left_Offset)
+                        end
+                        Left_Offset = Left_Offset + 10
+                    elseif Health_Position == "Right" then
+                        if Healthbar_Position == "Right" then
+                            Health.Position = Vector2.new(X_Maximal + Box_Size.X + 4 + 4 + Health.TextBounds.X / 2, Box_Position.Y + 2) - Vector2.new(Box_Size.X, -(100 * Health_Left_Size_Outline.Y / 100) + 2 - Right_Offset)
+                        else
+                            Health.Position = Vector2.new(X_Maximal + Box_Size.X + 3 + Health.TextBounds.X / 2, Box_Position.Y + 2) - Vector2.new(Box_Size.X, -(100 * Health_Left_Size_Outline.Y / 100) + 2 - Right_Offset)
+                        end
+                        Right_Offset = Right_Offset + 10
+                    end
+                    Health.Text = tostring(math.floor(Current_Health + 0.5))
+                    Health.Color = Health_Lerp_Color
+                    Health.OutlineColor = ESP.Settings.Health.OutlineColor
+                    Health.Visible = ESP.Settings.Health.Enabled
+
+                    -- Chams
+                    local Chams_Settings = ESP.Settings.Chams
+                    local Chams_Enabled = Chams_Settings.Enabled
+                    Chams.Enabled = Chams_Enabled
+                    Chams.Adornee = Chams_Enabled and Character or nil
+                    if Chams_Enabled then
+                        Chams.FillColor = Chams_Settings.Color
+                        Chams.OutlineColor = Chams_Settings.OutlineColor
+                        Chams.FillTransparency = Chams_Settings.Transparency
+                        Chams.FillColor = Chams_Settings.OutlineTransparency
+                    end
                 else
                     for Index, Drawing in pairs(self.Components) do
                         Drawing.Visible = false
@@ -313,6 +377,8 @@ do -- ESP Functions
         Components.Name = Framework:Draw("Text", {Text = Instance.Name, Font = 2, Size = 13, Outline = true, Center = true})
         Components.Distance = Framework:Draw("Text", {Font = 2, Size = 13, Outline = true, Center = true})
         Components.Tool = Framework:Draw("Text", {Font = 2, Size = 13, Outline = true, Center = true})
+        Components.Health = Framework:Draw("Text", {Font = 2, Size = 13, Outline = true, Center = true})
+        Components.Chams = Framework:Instance("Highlight", {DepthMode = Enum.HighlightDepthMode.AlwaysOnTop})
         self.Objects[Instance] = Object
         return Object
     end
